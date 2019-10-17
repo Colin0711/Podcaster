a4793fada76316484aadc57f4a94810d58d51ac8
Colin0711 <55683859+Colin0711@users.noreply.github.com>
2019-10-17 16:21:15 +0200

    Add files via upload

---
+++ RssReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Podcaster_Projekt.Model
+{
+    public class RssInformation
+    {
+        public string Title;
+        public string PublicationDate;
+        public string Description;
+        public string Length;
+    }
+
+    public class RssReader
+    {
+        public static  List<RssInformation> Read(string url)
+        {
+            var webClient = new WebClient();
+            webClient.Headers.Add("Podcaster by Volkan Javuz, Teja Finkbeiner, Colin Rieger");
+
+            string xmlData = webClient.DownloadString(url);
+
+            XDocument xml = XDocument.Parse(xmlData);
+
+            var Feed = (from descendant in xml.Descendants("item")
+                        select new RssInformation()
+                        {
+                            Description = descendant.Element("description").Value,
+                            Title = descendant.Element("title").Value,
+                            PublicationDate = descendant.Element("pubDate").Value,
+                            Length = descendant.Element("length").Value,
+                    }).ToList();
+
+            return Feed;
+        }
+
+        public class GetRssFeed
+        {
+            public List<RssInformation> RSSFeed { get; set; }
+        }
+    }
+}
